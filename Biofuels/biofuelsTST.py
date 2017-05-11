b6582b8261ac6594d28484a6a2041f65114ff8cd
nateharms <harms.n@husky.neu.edu>
2017-05-11 16:59:11 -0400

    Got the biofuelsTST.py script working. Currently running on discovery

--- Biofuels/biofuelsTST.py
+++ Biofuels/biofuelsTST.py
@@ -57,13 +57,17 @@ logging.info('RMG Database Loaded')
 
 loadSpecies = rmgDatabase.kinetics.families[rxnFamilies[0]]
 species_dict_file = os.path.expanduser('~/Code/OOHabstraction/Biofuels/chemkin/species_dictionary.txt')
+species_dict = loadSpecies.getSpecies(species_dict_file)
 logging.info("Species Dictionary loaded")
+print species_dict
 
 file_object = open(os.path.expanduser('~/Code/OOHabstraction/Biofuels/chemkin/chem_annotated.inp'), 'r')
 mechLines = file_object.readlines()
 logging.info("Chemkin loaded")
 
-rxnList = {}
+
+
+rxnList = []
 gotit = []
 for rxnFamily in rxnFamilies:
 	for k, line in enumerate(mechLines):
@@ -74,32 +78,34 @@ for rxnFamily in rxnFamilies:
 					break
 			if reaction not in gotit:
 				gotit.append(reaction)
-				rxnList[reaction] = rxnFamily
+				rxnList.append((rxnFamily, mechLines[k+m]))
 logging.info(rxnList)
 logging.info(type(rxnList))
+"""
 def sorter_key(rxn):
-    """
+
     A key to generate the sort order for reactions.
     Needs to be consistent across runs, but we want small reactions
     first, so that it runs faster.
-    """
+
     weight = 0.
     for r in rxn.reactants:
         weight += r.molecule[0].getMolecularWeight()
     return (weight, repr(rxn))
+"""
 
-allRxns = sorted(rxnList.keys(), key=sorter_key)
-chemkinRxn = allRxns[i - 1]
-logging.info("This reaction:")
-logging.info(str(chemkinRxn))
-logging.info(repr(chemkinRxn))
 
 
 logging.info("Reaction list generated")
 reactionTuple = rxnList[i-1]
 rxnFamily, reactionLine = reactionTuple
-rxnFormula, A, n, Ea = reactionLine.split()
-reactants, products = rxnFormula.split('=')
+chemkinRxn, A, n, Ea = reactionLine.split()
+reactants, products = chemkinRxn.split('=')
+logging.info("This reaction:")
+logging.info(str(chemkinRxn))
+logging.info(repr(chemkinRxn))
+logging.info(reactants)
+logging.info(products)
 if rxnFamily in ['H_Abstraction']:
 	rSpecies1, rSpecies2 = [species_dict[j] for j in reactants.split('+')]
 	pSpecies1, pSpecies2 = [species_dict[j] for j in products.split('+')]
@@ -156,11 +162,6 @@ def sorter_key(rxn):
         weight += r.molecule[0].getMolecularWeight()
     return (weight, repr(rxn))
 
-allRxns = sorted(kineticsDict.keys(), key=sorter_key)
-chemkinRxn = allRxns[i - 1]
-logging.info("This reaction:")
-logging.info(str(chemkinRxn))
-logging.info(repr(chemkinRxn))
 
 
 qmCalc = QMCalculator(
@@ -198,27 +199,33 @@ def performCalcs(chemkinRxn):
     # Ensure all resonance isomers have been generated
     logging.info('Ensuring resonance isomers have been generated')
     for species in itertools.chain(chemkinRxn.reactants, chemkinRxn.products):
-        species.molecule = species.molecule[0].generateResonanceIsomers()
+        print(type(species))
+        species = species.generateResonanceIsomers()
 
     testReaction = Reaction(reactants=chemkinRxn.reactants, products=chemkinRxn.products, reversible=True)
 
-    reactant_molecules = [species.molecule for species in chemkinRxn.reactants]
+    reactants = [species for species in chemkinRxn.reactants]
     # reactant_molecules is a list of lists of resonance isomers,
     # eg. a bimolecular reaction where the second reactant has 2 isomers is: [[r1],[r2i1,r2i2]]
 
-    products = [species.molecule[0] for species in chemkinRxn.products]
+    products = [species for species in chemkinRxn.products]
     # products is a list of molecule objects (only one resonance form of each product), eg [p1, p2]
-    for reactants in itertools.product(*reactant_molecules):
-        # reactants is now a tuple of molecules, one for each reactant,  eg. (r1, r2i1)
-        checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactants, products, only_families=rxnFamilies)
-        if len(checkRxn) == 1:
-            break
+    print reactants
+    #print type(reactant_molecules[0])
+    print products
+
+    checkRxn = rmgDatabase.kinetics.generateReactionsFromFamilies(reactants, products, only_families=rxnFamilies)
+    if len(checkRxn) == 1:
+        logging.info("Generated one reaction:")
+        logging.info(checkRxn)
+        reaction = checkRxn[0]
     else:  # didn't break from for loop
         for reaction in checkRxn:
             logging.info("Generated these reactions:")
             logging.info(reaction)
+            reaction = checkRxn[0]
         raise Exception("Couldn't generate exactly one reaction matching {} in family {}".format(chemkinRxn, rxnFamilies))
-    reaction = checkRxn[0]
+
     logging.info("The reaction of interest is as follows: ")
     logging.info(reaction)
 
@@ -347,134 +354,5 @@ def performCalcs(chemkinRxn):
         input_string = ','.join(row)
         with open(os.path.join(folderPath, smiles_dict[entry] + '_kinetics.txt'), 'w') as kinTxt:
                 kinTxt.write(input_string)
-
-performCalcs(chemkinRxn)
-
-
-"""if not gotOne:
-	print "No reactions found for reaction {4}: {0} + {1} = {2} + {3}".format(rSpecies1.molecule[0].toSMILES(), rSpecies2.molecule[0].toSMILES(), pSpecies1.molecule[0].toSMILES(), pSpecies2.molecule[0].toSMILES(), i)
-else:
-	qmCalc = QMCalculator(
-									software='gaussian',
-									method='m062x',
-									fileStore='/gss_gpfs_scratch/harms.n/QMfiles',
-									scratchDirectory='/gss_gpfs_scratch/harms.n/QMscratch',
-									)
-	calculate(reaction)"""
-
-"""
-#~~~~~~~~~~~
-import os
-import sys
-
-import rmgpy
-from rmgpy.molecule import Molecule
-from rmgpy.reaction import Reaction
-from rmgpy.species import Species
-from rmgpy.data.kinetics import KineticsFamily, ReactionRecipe, KineticsDatabase
-from rmgpy.data.rmg import RMGDatabase
-from rmgpy.qm.main import QMCalculator
-from rmgpy.qm.gaussian import GaussianTSB3LYP
-
-if len(sys.argv)>1:
-	i = int(sys.argv[-1])
-elif os.getenv('SLURM_ARRAY_TASK_ID'):
-	i = int(os.getenv('SLURM_ARRAY_TASK_ID'))
-else:
-	raise Exception("Specify a TS number!")
-
-rxnFamilies = ['H_Abstraction']#,'Cl-Abstraction' ['intra_H_migration', 'R_Addition_MultipleBond', 'H_Abstraction', 'Disproportionation']
-
-print 'Loading RMG Database ...'
-rmgDatabase = RMGDatabase()
-rmgDatabase.load(os.path.abspath(os.path.join(os.getenv('RMGpy'), '..', 'RMG-database', 'input')), kineticsFamilies=rxnFamilies, seedMechanisms=[], solvation=False)
-print 'Finished loading RMG Database ...'
-
-# Doesn't matter which family, it loads the entire species dict
-loadSpecies = rmgDatabase.kinetics.families[rxnFamilies[0]]
-species_dict_file = os.path.expanduser('~/Code/OOHabstraction/Biofuels/chemkin/species_dictionary.txt')
-
-file_object = open(os.path.expanduser('~/Code/OOHabstraction/Biofuels/chemkin/chem_annotated.inp'), 'r')
-mechLines = file_object.readlines()
-
-rxnList = []
-gotit = []
-for rxnFamily in rxnFamilies:
-	for k, line in enumerate(mechLines):
-		if line.startswith('! Template reaction: {0}'.format(rxnFamily)):
-			for m in range(10):
-				reaction = mechLines[k+m].split()[0]
-				if not reaction.startswith('!'):
-					break
-			if reaction not in gotit:
-				gotit.append(reaction)
-				rxnList.append((rxnFamily, mechLines[k+m]))
-
-reactionTuple = rxnList[i-1]
-rxnFamily, reactionLine = reactionTuple
-rxnFormula, A, n, Ea = reactionLine.split()
-reactants, products = rxnFormula.split('=')
-if rxnFamily in ['H_Abstraction']:
-	rSpecies1, rSpecies2 = [species_dict[j] for j in reactants.split('+')]
-	pSpecies1, pSpecies2 = [species_dict[j] for j in products.split('+')]
-
-	rSpecies1.generateResonanceIsomers()
-	rSpecies2.generateResonanceIsomers()
-	pSpecies1.generateResonanceIsomers()
-	pSpecies2.generateResonanceIsomers()
-	testReaction = Reaction(reactants=[rSpecies1, rSpecies2], products=[pSpecies1, pSpecies2], reversible=True)
-	reactionList = []
-	for moleculeA in rSpecies1.molecule:
-		for moleculeB in rSpecies2.molecule:
-			tempList = rmgDatabase.kinetics.generateReactionsFromFamilies([moleculeA, moleculeB], [], only_families=[rxnFamily])
-			for rxn0 in tempList:
-				reactionList.append(rxn0)
-print "Reaction lists made"
-gotOne=False
-for reaction in reactionList:
-	# Check if any of the RMG proposed reactions matches the reaction in the mechanism
-	if testReaction.isIsomorphic(reaction):
-		# Now add the labeled atoms to the Molecule, and check all labels were added
-		atLblsR = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
-		atLblsP = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
-
-		for reactant in reaction.reactants:
-
-			reactant.clearLabeledAtoms()
-			for atom in reactant.atoms:
-				for atomLabel in reaction.labeledAtoms:
-					if atom==atomLabel[1]:
-						atom.label = atomLabel[0]
-						atLblsR[atomLabel[0]] = True
-		for product in reaction.products:
-
-			product.clearLabeledAtoms()
-			for atom in product.atoms:
-				for atomLabel in reaction.labeledAtoms:
-					if atom==atomLabel[1]:
-						atom.label = atomLabel[0]
-						atLblsP[atomLabel[0]] = True
-		if all( atLblsR.values() ) and all( atLblsP.values() ):
-			gotOne=True
-			break
-
-def calculate(reaction):
-	rxnFamily = reaction.family
-	tsDatabase = rmgDatabase.kinetics.families[rxnFamily].transitionStates
-	reaction = qmCalc.getKineticData(reaction, tsDatabase)
-
-	for files in os.listdir('./'):
-		if files.startswith('core'):
-			os.remove(files)
-
-if not gotOne:
-	print "No reactions found for reaction {4}: {0} + {1} = {2} + {3}".format(rSpecies1.molecule[0].toSMILES(), rSpecies2.molecule[0].toSMILES(), pSpecies1.molecule[0].toSMILES(), pSpecies2.molecule[0].toSMILES(), i)
-else:
-	qmCalc = QMCalculator(
-									software='gaussian',
-									method='m062x',
-									fileStore='/gss_gpfs_scratch/harms.n/QMfiles',
-									scratchDirectory='/gss_gpfs_scratch/harms.n/QMscratch',
-									)
-	calculate(reaction)
-"""
+rxn = reactionList[0]
+performCalcs(rxn)
