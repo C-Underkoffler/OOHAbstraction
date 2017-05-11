5cd38590a72347932a4a9ee5ae9956ea9c138c39
Nate Harms <harms.n@husky.neu.edu>
2017-05-11 14:31:47 -0400

    Pre-merge commit

--- Biofuels/biofuelsTST.py
+++ Biofuels/biofuelsTST.py
@@ -75,7 +75,7 @@ for reaction in reactionList:
 		atLblsP = dict([(lbl[0], False) for lbl in reaction.labeledAtoms])
 
 		for reactant in reaction.reactants:
-			reactant = reactant[0]
+
 			reactant.clearLabeledAtoms()
 			for atom in reactant.atoms:
 				for atomLabel in reaction.labeledAtoms:
@@ -83,7 +83,7 @@ for reaction in reactionList:
 						atom.label = atomLabel[0]
 						atLblsR[atomLabel[0]] = True
 		for product in reaction.products:
-			product = product[0]
+
 			product.clearLabeledAtoms()
 			for atom in product.atoms:
 				for atomLabel in reaction.labeledAtoms:
